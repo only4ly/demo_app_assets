f94d7d94c55eea9b2d27308666ab98ca5cd00202
zhangw <zhangw@zhangwdeMacBook-Pro.local>
2018-06-27 22:30:08 +0800

    compress

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
- 
+const compress = require('koa-compress') 
+
 // or use absolute paths
+app.use(compress())
 app.use(serve(__dirname + '/assets'));
  
 app.listen(3000);
